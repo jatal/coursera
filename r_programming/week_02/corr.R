99960fcb3d66ef4b7ddfe902f7a8bff251f5e4da
crowle <craig.rowley@nike.com>
2014-12-09 15:43:25 -0800

    removed the rounding since it prevented a correct automated submission

--- r_programming/week_02/corr.R
+++ r_programming/week_02/corr.R
@@ -10,8 +10,11 @@ corr <- function(directory, threshold = 0, precision=5) {
     s <- sprintf("%03d",id)
     file <- paste(directory,"/",s,".csv",sep="")
     df <- read.csv(file)
-    corrl <- cor(df$sulfate, df$nitrate, use="complete.obs")
-    corrls <- cbind(corrls, round(corrl,precision) )
+    corrl <- cor(df$sulfate, df$nitrate, use="pairwise.complete.obs")
+    #corrls <- cbind(corrls, round(corrl,precision) )
+    
+    corrls <- cbind(corrls, corrl)
+    
   }
   
   as.vector(corrls)
